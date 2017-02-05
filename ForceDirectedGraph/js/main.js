3fc11846b1c50d17efdf43a16b67d6a55f08cb4a
Lai Kitman <alanljw12345@gmail.com>
2017-02-05 15:21:23 +0800

    [REFACTOR] clean up code

--- ForceDirectedGraph/js/main.js
+++ ForceDirectedGraph/js/main.js
@@ -21,11 +21,11 @@ $(document).ready(
             let clickedShape;
             let tool = new Tool();
             // when mouse is no longer click on the shape, release.
-            tool.onMouseUp = function (event) {
+            tool.onMouseUp = function () {
                 clickedShape = undefined;
             };
             tool.activate();
-            nodes.forEach(function (node, index, array) {
+            nodes.forEach(function (node) {
                 // randomize the initial position
                 node.circle = new Shape.Circle(Point.random().multiply(canvasSize), radius);
                 node.circle.strokeColor = 'white';
@@ -40,59 +40,38 @@ $(document).ready(
                 };
             });
 
-            let fromNode, toNode;
-            links.forEach(function (link, index, linksArray) {
-                nodes.forEach(function (node, index, array) {
-                    if (node.id == link.source) {
-                        fromNode = node;
-                    }
-                });
+            updatePath(links, nodes);
 
-                nodes.forEach(function (node, index, array) {
-                    if (node.id == link.target) {
-                        toNode = node;
-                    }
-                });
-
-                link.path = new Path([fromNode.circle.position, toNode.circle.position]);
-                link.path.strokeColor = link.path.fillColor = pathColor;
-                link.path.strokeWidth = Math.sqrt(link.value);
-            });
-
-
-            view.onFrame = function (event) {
-                nodes.forEach(function (nodeOuter, index, nodesList) {
+            view.onFrame = function () {
+                nodes.forEach(function (nodeOuter) {
                     nodeOuter.force = new Point(0, 0);
                     // repel force
-                    nodes.forEach(function (nodeInner, index, nodesList) {
-                        if (nodeInner.id !== nodeOuter.id) {
+                    nodes.filter((node) => node.id !== nodeOuter.id)
+                        .forEach(function (nodeInner) {
                             let vector = nodeOuter.circle.position.subtract(nodeInner.circle.position);
                             vector.length = calculateRepulsionForce(vector.length);
                             nodeOuter.force = nodeOuter.force.add(vector);
-                        }
-                    });
+                        });
 
-                    links.forEach(function (link, index, array) {
-                        if (link.source == nodeOuter.id) {
-                            nodes.forEach(function (nodeInner, index, nodesList) {
-                                if (nodeInner.id == link.target) {
+                    links.filter((link) => link.source === nodeOuter.id)
+                        .forEach(function (link) {
+                            nodes.filter((node) => node.id === link.target)
+                                .forEach(function (nodeInner) {
                                     let vector = nodeInner.circle.position.subtract(nodeOuter.circle.position);
                                     vector.length = calculateAttractionForce(vector.length);
                                     nodeOuter.force = nodeOuter.force.add(vector);
-                                }
-                            });
-                        }
+                                });
+                        });
 
-                        if (link.target == nodeOuter.id) {
-                            nodes.forEach(function (nodeInner, index, nodesList) {
-                                if (nodeInner.id == link.source) {
+                    links.filter((link) => link.target === nodeOuter.id)
+                        .forEach(function (link) {
+                            nodes.filter((node) => node.id == link.source)
+                                .forEach(function (nodeInner) {
                                     let vector = nodeInner.circle.position.subtract(nodeOuter.circle.position);
                                     vector.length = calculateAttractionForce(vector.length);
                                     nodeOuter.force = nodeOuter.force.add(vector);
-                                }
-                            });
-                        }
-                    });
+                                });
+                        });
                     // if the shape is clicked, just leave it there and let mouse control its position
                     if (nodeOuter.circle !== clickedShape) {
                         nodeOuter.circle.position = nodeOuter.circle.position.add(nodeOuter.force.multiply(C4));
@@ -111,21 +90,27 @@ $(document).ready(
 
             function updatePath(links, nodes) {
                 let fromNode, toNode;
-                links.forEach(function (link, index, linksArray) {
-                    nodes.forEach(function (node, index, array) {
-                        if (node.id == link.source) {
+                links.forEach(function (link) {
+                    nodes.filter((node) => node.id == link.source)
+                        .forEach(function (node) {
                             fromNode = node;
-                        }
-                    });
+                        });
 
-                    nodes.forEach(function (node, index, array) {
-                        if (node.id == link.target) {
+                    nodes.filter((node) => node.id == link.target)
+                        .forEach(function (node) {
                             toNode = node;
-                        }
-                    });
+                        });
 
-                    link.path.setSegments([fromNode.circle.position, toNode.circle.position]);
+                    // init
+                    if (link.path === undefined) {
+                        link.path = new Path([fromNode.circle.position, toNode.circle.position]);
+                        link.path.strokeColor = link.path.fillColor = pathColor;
+                        link.path.strokeWidth = Math.sqrt(link.value);
+                    } else {
+                        link.path.setSegments([fromNode.circle.position, toNode.circle.position]);
+                    }
                 });
+
             }
         });
     }
